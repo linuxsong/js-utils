e3c4732e8125cffdad4a85ac1cea9837dbbc402f
linuxsong <linuxsong@gmail.com>
2016-03-16 15:24:51 +0800

    Create js-utils.js

---
+++ js-utils.js
@@ -0,0 +1,20 @@
+        /*
+        Resize image size with same ratio.
+        useage: 
+                $(function() {
+                $("img").one("load", function() {
+                        resizeImage(320,320, this);
+                }).each(function() {
+                        if(this.complete) $(this).load();
+                });
+        });
+        */
+        function resizeImage(maxWidth, maxHeight, objImg)
+        {
+                var img = new Image();
+                img.src = objImg.src;
+                if (img.width <= maxWidth && img.height <= maxHeight) return;
+                var minRatio = Math.min(maxWidth/img.width, maxHeight/img.height);
+                objImg.width = img.width * minRatio;
+                objImg.height = img.height * minRatio;
+        }
